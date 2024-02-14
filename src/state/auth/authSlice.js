0ff07a31b88977f907936fe481b5294081b5f741
laitouss-yassine <yassinelaitouss2380@gmail.com>
2024-02-14 13:10:51 +0100

    redux store and auth slice to test the login

---
+++ src/state/auth/authSlice.js
@@ -0,0 +1,30 @@
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+    user: "",
+    isLogged: false
+}
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase()
+    }
+})
+
+export const login = createAsyncThunk(
+    "auth/login",
+    async (data) => {
+        const {email, password} = data;
+        await axios.post('http://localhost:8090/api/auth/login', {
+            email,
+            password
+        })
+    }
+)
+
+
+export default authSlice.reducer;
