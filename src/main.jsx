e3e9eb67c648bcfb75e02b25c3631a69caaa7085
laitouss-yassine <yassinelaitouss2380@gmail.com>
2024-03-13 23:04:13 +0000

    resolve importation of state in redux soore

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import {Provider} from 'react-redux'
-import store from './state/store.js'
+import store from '@/state/Store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
